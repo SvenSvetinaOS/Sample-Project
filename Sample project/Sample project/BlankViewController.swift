c1422998ed367638f31798bdde92e15dde762962
Sven Svetina <sven.svetina@five.agency>
2019-10-09 10:52:29 +0200

    Added TableView for users, cell button for user info and alert placeholder
    TODO:
    1. Refactor to MVVM
    2. Add user info ViewController

---
+++ Sample project/Sample project/BlankViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BlankViewController.swift
+//  Sample project
+//
+//  Created by Sven Svetina on 04/10/2019.
+//  Copyright © 2019 Sven Svetina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BlankViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Sample project/Sample project/ViewController.swift
+++ Sample project/Sample project/ViewController.swift
@@ -8,26 +8,70 @@
 
 import UIKit
 import Foundation
+import PureLayout
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var jsonService = JsonService()
     
     var dataArray = [UserDetails]()
     
     @IBOutlet weak var fetchButton: UIButton!
+    @IBOutlet weak var userTableView: UITableView!
     
     
     @IBAction func buttonTapped(_ sender: Any) {
-        getData()
+        //        getData()
+        
+        jsonService.fetchData() { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    print(error)
+                case .success(let data):
+                    self.dataArray = data
+                }
+                self.userTableView.reloadData()
+                self.userTableView.isHidden = false
+            }
+            
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        userTableView.isHidden = true
+        userTableView.dataSource = self
+        userTableView.delegate = self
+        
+        
+        userTableView.register(UINib(nibName: "UserTable", bundle: nil), forCellReuseIdentifier: "userCell")
         // Do any additional setup after loading the view.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = userTableView.dequeueReusableCell(withIdentifier: "userCell") as! UserTable
+        let user = self.dataArray[indexPath.row]
+        
+        cell.titleLabel?.text = user.name
+        //        cell.titleLabel?.textAlignment = .left
+        cell.titleLabel?.autoPinEdge(.left, to: .left, of: cell, withOffset: 8)
+        cell.titleLabel?.autoPinEdge(.right, to: .right, of: cell)
+        cell.titleLabel?.autoPinEdge(.top, to: .top, of: cell)
+        cell.titleLabel?.autoPinEdge(.bottom, to: .bottom, of: cell)
+        cell.cellButton?.addTarget(self, action: #selector(ViewController.cellButtonTapped), for: .touchUpInside)
+        
+        return cell
+    }
     
     func getData() {
         jsonService.fetchData() { result in
@@ -38,9 +82,18 @@ class ViewController: UIViewController {
                 self.dataArray = data
                 let name = data.first!.name
                 let address = data.first!.address
-                print(name, address)
+                let company = data.first!.company
+                print(name, address, company)
             }
         }
+    }
+    @objc func cellButtonTapped (_ sender: UIButton) {
+        let alert = UIAlertController(title: "Error", message: "Not implemented yet", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Cancel", style: .cancel) { (action:UIAlertAction) in
+            print("You've pressed cancel");
+        }
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
