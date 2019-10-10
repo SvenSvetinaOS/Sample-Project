22a0cc1c2a984caaa60a21dcc3aba5a62859ac79
Sven Svetina <sven.svetina@five.agency>
2019-10-10 15:42:20 +0200

    Had problems with GitHub. Created new-develop

---
+++ Sample project/Sample project/ViewModels/UserDetailsViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  UserDetailsViewModel.swift
+//  Sample project
+//
+//  Created by Sven Svetina on 10/10/2019.
+//  Copyright © 2019 Sven Svetina. All rights reserved.
+//
+
+import Foundation
+
+public class UserDetailsViewModel {
+    
+    private let userDetails: UserDetails
+    private let addressInfo: AddressInfo
+    private let geoInfo: GeoInfo
+    private let companyInfo: CompanyInfo
+    
+    
+    init(userDetails: UserDetails,
+         addressInfo: AddressInfo,
+         geoInfo: GeoInfo,
+         companyInfo: CompanyInfo)
+    {
+        self.userDetails = userDetails
+        self.addressInfo = addressInfo
+        self.geoInfo = geoInfo
+        self.companyInfo = companyInfo
+    }
+    public var id: Int {
+        return userDetails.id
+    }
+    public var name: String {
+        return userDetails.name
+    }
+    
+    public var phone: String {
+        return userDetails.phone
+    }
+    public var website: String {
+        return userDetails.website
+    }
+}
